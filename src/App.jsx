1affa49264aeac4ffe36a93b78be6edb2988cb32
oxideclop <thanatad-b@rmutp.ac.th>
2019-05-09 19:50:47 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class App extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <h4>Hello~!</h4>
+            </React.Fragment>
+        )
+    }
+}
+
+export default App
